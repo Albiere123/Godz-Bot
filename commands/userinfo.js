9b0a27272b0c68b424988ea4f555738ff026c085
Albiere <albieregames30@gmail.com>
2020-05-07 20:37:08 -0300

    ;-;

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -3,10 +3,11 @@ const Discord = require('discord.js')
 exports.run = async(client, message, args) => {
     let member = message.mentions.members.first()
     if(!member) {
-        let embed  = new Discord.messageembed()
+        let embed  = new Discord.MessageEmbed()
         .setTitle(`${message.author.username}`)
         .addField("ID", `${message.author.id}`)
         .addField("tag", `${message.author.tag}`)
+        .setThumbnail(message.author.displayavatarURL)
         message.channel.send(embed)
     }
     
